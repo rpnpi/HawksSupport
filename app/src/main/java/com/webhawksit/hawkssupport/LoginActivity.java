6ff23f65b521c05ca51e4415a559211fff5d59e5
Rio <Rio>
2016-07-29 18:05:01 +0600

    four Push

--- app/src/main/java/com/webhawksit/hawkssupport/LoginActivity.java
+++ app/src/main/java/com/webhawksit/hawkssupport/LoginActivity.java
@@ -1,32 +1,26 @@
 package com.webhawksit.hawkssupport;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class MyProfileActivity extends AppCompatActivity {
-
-    TextView textView ;
+public class LoginActivity extends AppCompatActivity {
+TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_profile);
-        Typeface font = Typeface.createFromAsset(getAssets(),"fonts/fontawesome-webfont.ttf");
-        TextView textView = (TextView) findViewById(R.id.getStart);
-        textView.setTypeface(font);
-        textView.setText("Get Started    " + "\uF061");
+        setContentView(R.layout.activity_login);
+
+        TextView textView = (TextView) findViewById(R.id.signUpFooter);
 
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
                 startActivity(intent);
             }
         });
-
-
     }
 }
