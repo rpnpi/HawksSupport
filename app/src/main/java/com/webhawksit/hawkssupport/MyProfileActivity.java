2dcfb86109d26707014dcce4c8fba370a8e9eaca
rpnpi <rayhan.edu.bd@gmail.com>
2016-08-16 17:56:16 +0600

    dashboard working continue

--- app/src/main/java/com/webhawksit/hawkssupport/MyProfileActivity.java
+++ app/src/main/java/com/webhawksit/hawkssupport/MyProfileActivity.java
@@ -9,20 +9,35 @@ import android.widget.TextView;
 
 public class MyProfileActivity extends AppCompatActivity {
 
-    TextView textView ;
+    TextView textView, firstName, lastName, email, phoneNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
-        Typeface font = Typeface.createFromAsset(getAssets(),"fonts/fontawesome-webfont.ttf");
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/fontawesome-webfont.ttf");
         TextView textView = (TextView) findViewById(R.id.getStart);
         textView.setTypeface(font);
         textView.setText("Get Started    " + "\uF061");
 
+        String firstname = getIntent().getStringExtra("firstName");
+        firstName = (TextView) findViewById(R.id.firstNameView);
+        firstName.setText(firstname);
+
+        lastName = (TextView) findViewById(R.id.lastNameView);
+        lastName.setText(getIntent().getStringExtra("lastName"));
+
+        email = (TextView) findViewById(R.id.emailView);
+        email.setText(getIntent().getStringExtra("email"));
+
+        phoneNumber = (TextView) findViewById(R.id.phoneNumberView);
+        String phone = getIntent().getStringExtra("phoneNumber");
+        phoneNumber.setText("0"+phone);
+
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                Intent intent = new Intent(getApplicationContext(), Dashboard.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/webhawksit/hawkssupport/SignUpActivity.java
+++ app/src/main/java/com/webhawksit/hawkssupport/SignUpActivity.java
@@ -5,26 +5,52 @@ import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SignUpActivity extends AppCompatActivity {
     TextView textView, footerTextView;
+    EditText firstName, lastName, email, password, phoneNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        Typeface font = Typeface.createFromAsset( getAssets(), "fonts/fontawesome-webfont.ttf" );
-        TextView textView = (TextView) findViewById( R.id.footerTextSingUp );
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/fontawesome-webfont.ttf");
+        TextView textView = (TextView) findViewById(R.id.footerTextSingUp);
         textView.setTypeface(font);
-        textView.setText("My profile   "+"\uf061");
+        textView.setText("My profile   " + "\uf061");
+
+        firstName = (EditText) findViewById(R.id.firstName);
+        if (firstName.getText().toString().length() == 0) {
+            firstName.setError("First name is required!");
+        }
+
+        lastName = (EditText) findViewById(R.id.lastName);
+        email = (EditText) findViewById(R.id.emailAddress);
+        phoneNumber = (EditText) findViewById(R.id.phoneNumber);
 
         TextView footerTextView = (TextView) findViewById(R.id.footerTextSingUp);
+
         footerTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),MyProfileActivity.class);
-                startActivity(intent);
+                if (!firstName.getText().toString().isEmpty()) {
+                    Intent intent = new Intent(getApplicationContext(), MyProfileActivity.class);
+
+                    intent.putExtra("firstName", firstName.getText().toString());
+                    intent.putExtra("lastName", lastName.getText().toString());
+                    intent.putExtra("email", email.getText().toString());
+                    intent.putExtra("phoneNumber", phoneNumber.getText().toString());
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "This not valid ", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
